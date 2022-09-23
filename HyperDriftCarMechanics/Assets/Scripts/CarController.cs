d66636fe6c72b95793c87907005dff63d7760e72
emiruyan <emir-uyn@hotmail.com>
2022-09-23 20:31:36 +0300

    Wheel Rotate

--- HyperDriftCarMechanics/Assets/Scripts/CarController.cs
+++ HyperDriftCarMechanics/Assets/Scripts/CarController.cs
@@ -14,6 +14,10 @@ public class CarController : MonoBehaviour
     private float dragAmount=0.99f;//Sürtünme kuvveti 
 
     private Vector3 _moveVec;
+    private Vector3 _rotVec;
+
+    public Transform lw, rw;//Ön tekerleklerin transformu
+    
 
 
     private void Update()
@@ -32,12 +36,19 @@ public class CarController : MonoBehaviour
         //Arabayı ileri doğru sürekli hareket ettirmek için değerler oluşturduk ve _moveVec'e atadık
         _moveVec += transform.forward * carSpeed * Time.deltaTime;
         transform.position += _moveVec * Time.deltaTime;//_moveVec'i deltaTime ile çarpıp transform'a atadık
+
+        _rotVec += new Vector3(0, Input.GetAxis("Horizontal"), 0);
         
         transform.Rotate(Vector3.up * Input.GetAxis("Horizontal")* steerAngle * Time.deltaTime *_moveVec.magnitude);
 
         _moveVec *= dragAmount;
         _moveVec = Vector3.ClampMagnitude(_moveVec, maxSpeed);//Arabanın hızını sınırladık
         _moveVec = Vector3.Lerp(_moveVec.normalized, transform.forward, Traction * Time.deltaTime) * _moveVec.magnitude; 
+        _rotVec = Vector3.ClampMagnitude(_rotVec, steerAngle);//Tekerlerin  dönüşünü sınırladık
+        
+        //localRotation girme sebebimiz tekerlerin arabanın içinde ayrı bir rotation'a sahip olması
+        lw.localRotation = Quaternion.Euler(_rotVec);
+        rw.localRotation = Quaternion.Euler(_rotVec);
 
 
     }
